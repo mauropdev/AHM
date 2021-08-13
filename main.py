0f5d1cf04dee2f08543a3305ba61db3bf93ceb60
Mauricio Perea Rodríguez <mauricio@pop-os.localdomain>
2021-08-13 15:52:42 -0500

    Create and List resources are added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 import uuid
 
 from datetime import datetime, timezone
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api, abort
 
 app = Flask(__name__)
@@ -47,7 +47,26 @@ class Measurement(Resource):
         abort(404, message=f'Measurement ID ={id} was not found')
 
 
+class MeasurementList(Resource):
+    def get(self):
+        return MEASUREMENTS, 200
+
+    def post(self):
+        data = json.loads(request.data)
+        measurement = {
+            'id': generate_uuid(),
+            'sys': data.get('sys'),
+            'dia': data.get('dia'),
+            'put': data.get('pul'),
+            'created': get_utc_now(),
+            'user_id': 'c0848663-1b9c-4a0b-a1e7-c1a783b4cb71'
+        }
+        MEASUREMENTS.append(measurement)
+        return measurement, 201
+
+
 api.add_resource(Measurement, '/v1/measurements/<string:id>')
+api.add_resource(MeasurementList, '/v1/measurements')
 
 if __name__ == '__main__':
     app.run(debug=True)
